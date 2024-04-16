66006cb4fd4d9c32fa3af0d74e803406640f875c
Sabrina Weber <contatosabrinaweber@gmail.com>
2024-04-16 20:06:54 -0300

    ajustes

--- javascript/slides.js
+++ javascript/slides.js
@@ -1,7 +1,18 @@
-let slideIndex = 0;
+/*let slideIndex = 0;
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  const slides = document.querySelectorAll('.slides li');
+  
+  if (slideIndex === 0 && n === -1) {
+    slideIndex = slides.length - 1; // Se estivermos no primeiro slide e clicarmos para voltar, vamos para o último
+  } else {
+    slideIndex = (slideIndex + n) % slides.length;
+    if (slideIndex < 0) {
+      slideIndex = slides.length - 1; // Se chegarmos ao início, vamos para o último
+    }
+  }
+  
+  showSlides();
 }
 
 function currentSlide(n) {
@@ -39,6 +50,51 @@ function updateDots(n) {
 }
 
 // Inicia a exibição dos slides
+showSlides();*/
+
+
+
+let slideIndex = 0;
+
+function plusSlides(n) {
+  slideIndex = (slideIndex + n) % slides.length;
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1; // Se chegarmos ao início, vamos para o último
+  }
+  showSlides();
+}
+
+function currentSlide(n) {
+  slideIndex = n - 1;
+  showSlides();
+}
+
+function showSlides() {
+  slides.forEach(slide => {
+    slide.style.display = "none";
+  });
+  
+  if (window.innerWidth < 600) {
+    slides[slideIndex % slides.length].style.display = "block";
+  } else {
+    for (let i = slideIndex; i < slideIndex + 3; i++) {
+      slides[i % slides.length].style.display = "block";
+    }
+  }
+  
+  dots.forEach(dot => dot.classList.remove('active'));
+  dots[slideIndex % dots.length].classList.add('active');
+}
+
+function updateDots(n) {
+  currentSlide(n);
+}
+
+const slides = document.querySelectorAll('.slides li');
+const dots = document.querySelectorAll('.dot');
+
 showSlides();
+
+
 
 
